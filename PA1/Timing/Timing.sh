4aeda4ceaaa1c16c173e4a38c42ef143dfe88301
Tyler DeFoor <tylerleedefoor@gmail.com>
2017-02-19 18:21:22 -0800

    Minor changes for Two_box and Timing
    Changed makefile and shell script for Timing and Two_box

---
+++ PA1/Timing/Timing.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -n 2
+#SBATCH -N 1
+#SBATCH --mem=2048MB
+#SBATCH --time=00:05:00
+#SBATCH --mail-user=tylerleedefoor@gmail.com
+#SBATCH --mail-type=ALL
+#SBATCH --output=One_box.txt
+
+srun One_box
