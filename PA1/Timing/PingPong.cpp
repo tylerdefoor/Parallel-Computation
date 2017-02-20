d0c9bccde2a54635443fad039ac018dfd4df4be8
tylerdefoor <tylerleedefoor@gmail.com>
2017-02-20 15:42:06 -0800

    Timing is seg faulting?

--- PA1/Timing/PingPong.cpp
+++ PA1/Timing/PingPong.cpp
@@ -22,10 +22,9 @@ int main ( int argc, char* argv[] )
 {
     /* Variable Declarations */
     int taskid, worldSize;
-    int number = 10;
-    double start, end, total, average;
-    vector<double> times ();
-    vector<int> numbers (5000, 0);
+    double start, end, total;
+    vector<int> send ( 10000, 1 );
+    vector<int> receive ( 10000 );
     /* End of Variable Declarations */
 
     //Initialize MPI
@@ -55,8 +54,7 @@ int main ( int argc, char* argv[] )
     //Get Rank
     MPI_Comm_rank ( MPI_COMM_WORLD, &taskid );
 
-    //Ping pong back and forth 1000 times
-    for ( int i = 10; i <= 5000; i+= 10 )
+    for ( int i = 2000; i <= 3000; i+= 10 )
     {
         //If we are in the master task
         if ( taskid == MASTER )
@@ -65,20 +63,18 @@ int main ( int argc, char* argv[] )
             start = MPI_Wtime (  );
 
             //Send the number
-            MPI_Send ( &numbers, i, TYPE, SLAVE, TAG, MPI_COMM_WORLD );
+            MPI_Send ( &send, i, TYPE, SLAVE, TAG, MPI_COMM_WORLD );
 
             //Receive the number
-            MPI_Recv ( &numbers, i, TYPE, SLAVE, TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE );
+            MPI_Recv ( &receive, i, TYPE, SLAVE, TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE );
 
             //End the timer 
             end = MPI_Wtime (  );
+            receive.clear (  );
 
             //Get the total time
             total = end - start;
             
-            //Add this to the times
-            times.push_back ( total );
-
             //Block until both are finished
             MPI_Barrier ( MPI_COMM_WORLD );
 
@@ -89,10 +85,11 @@ int main ( int argc, char* argv[] )
         else if ( taskid == SLAVE )
         {
             //Receive the number
-            MPI_Recv ( &numbers, i, TYPE, MASTER, TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE );
+            MPI_Recv ( &receive, i, TYPE, MASTER, TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE );
+            receive.clear (  );
 
             //Send the number
-            MPI_Send ( &numbers, i, TYPE, MASTER, TAG, MPI_COMM_WORLD );
+            MPI_Send ( &send, i, TYPE, MASTER, TAG, MPI_COMM_WORLD );
 
             //Block until both are finished
             MPI_Barrier ( MPI_COMM_WORLD );
