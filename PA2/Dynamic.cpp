5a3db00457fd86ab0250f7d6e02f5a735d9e001b
Tyler L Defoor <tdefoor@h1.cse.unr.edu>
2017-03-14 22:02:35 -0700

    Working Dynamic

--- PA2/Dynamic.cpp
+++ PA2/Dynamic.cpp
@@ -67,37 +67,45 @@ int main ( int argc, char** argv )
         //Array to receive the calculated row
         unsigned char* receiver = new unsigned char[WIDTH];
 
+        cout << "Before the intial send" << endl;
         //Send each slave a row
         for ( int i = 0; i < numSlaves; i++ )
         {
             MPI_Send ( &rowsCalculated, NUM_OBJECTS, INT_TYPE, (i + 1), 0, MPI_COMM_WORLD );
             rowsCalculated++;
+            cout << "Sent row: " << rowsCalculated << endl;
         }
+
+        cout << "After the initial send" << endl;
 
         //While there are still rows to calculate
         while ( rowsCalculated < HEIGHT )
         {
+            cout << "Master waiting to receive" << endl;
             //Receive a row
-            MPI_Recv ( &receiver, WIDTH, CHAR_TYPE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status );
+            MPI_Recv ( receiver, WIDTH, CHAR_TYPE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status );
 
+                cout << "Received row: " << status.MPI_TAG << endl;
             //Put the row into the map
             for ( int i = 0; i < WIDTH; i++ )
             {
                 map[status.MPI_TAG][i] = receiver[i];
             }
 
+            cout << "Sneding row: " << rowsCalculated << endl;
             //Send the returning slave another row
             MPI_Send ( &rowsCalculated, NUM_OBJECTS, INT_TYPE, status.MPI_SOURCE, 0, MPI_COMM_WORLD );
 
             //Increase rowsCalculated
             rowsCalculated++;
+            cout << "Sent row: " << rowsCalculated << endl;
         }
 
         //Loop through all the slaves again
         for ( int i = 0; i < numSlaves; i++ )
         {
             //Receive a row
-            MPI_Recv ( &receiver, WIDTH, CHAR_TYPE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status );
+            MPI_Recv ( receiver, WIDTH, CHAR_TYPE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status );
 
             //Put the row into the map
             for ( int i = 0; i < WIDTH; i++ )
@@ -124,6 +132,7 @@ int main ( int argc, char** argv )
         //The master will send -1 when all rows are calculated
         while ( currentRow != -1 )
         {
+            cout << "Slave calculating row: " << currentRow << endl;
             //Loop through the row and calculate 
             for ( int column = 0; column < WIDTH; column++ )
             {
@@ -136,11 +145,16 @@ int main ( int argc, char** argv )
                 row[column] = calculate ( current );
             }
 
+            cout << "Slave calculated row, sending" << endl;
+
             //Send the row to master with the tag as the calculated row number
-            MPI_Send ( &row, WIDTH, CHAR_TYPE, MASTER, currentRow, MPI_COMM_WORLD );
+            MPI_Send ( row, WIDTH, CHAR_TYPE, MASTER, currentRow, MPI_COMM_WORLD );
+
+            cout << "Slave sent" << endl;
 
             //Get the next row
             MPI_Recv ( &currentRow, 1, INT_TYPE, MASTER, 0, MPI_COMM_WORLD, &status );
+            cout << "Slave receiving" << endl;
         }
     }
 }
--- PA2/Dynamic.sh
+++ PA2/Dynamic.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH -n 9
+#SBATCH -n 3
 #SBATCH --mem=2048MB
-#SBATCH --time=00:10:00
+#SBATCH --time=00:05:00
 #SBATCH --mail-user=tylerleedefoor@gmail.com
 #SBATCH --mail-type=ALL
 #SBATCH --output=Dynamic.txt
