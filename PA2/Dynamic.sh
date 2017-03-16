6f46103934cf8393eaacbb21389b655260e0243a
Tyler DeFoor <tylerleedefoor@gmail.com>
2017-03-16 13:59:45 -0700

    Structured directory correctly

--- PA2/Dynamic.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH -n 16
-#SBATCH --mem=2048MB
-#SBATCH --time=00:05:00
-#SBATCH --mail-user=tylerleedefoor@gmail.com
-#SBATCH --mail-type=ALL
-#SBATCH --output=Dynamic.txt
-
-srun Dynamic
--- PA2/README.md
+++
@@ -1,5 +0,0 @@
-#Tyler DeFoor
-# PA2 - Mandelbrot
-
-##Mandelbrot_seq
-A sequential implementation of Mandelbrot. Outputs in black and white to "Mandelbrot_seq.out"
