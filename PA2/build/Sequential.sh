90d6fc6b6230f43c2176917477a2ead0106ffa2d
Tyler DeFoor <tylerleedefoor@gmail.com>
2017-03-16 14:06:29 -0700

    Added build folder

---
+++ PA2/build/Sequential.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -n 1
+#SBATCH --mem=2048MB
+#SBATCH --time=00:05:00
+#SBATCH --mail-user=tylerleedefoor@gmail.com
+#SBATCH --mail-type=ALL
+#SBATCH --output=Sequential.txt
+
+srun Sequential
