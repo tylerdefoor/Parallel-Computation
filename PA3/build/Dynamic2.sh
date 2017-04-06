a9783d7820946bde5b52e03447f72de0b98c82d0
tylerdefoor <tdefoor@h1.cse.unr.edu>
2017-04-05 21:39:08 -0700

    Data

---
+++ PA3/build/Dynamic2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#SBATCH --time=00:05:00
+#SBATCH -n 2
+
+totalNums=300000
+while [ $totalNums -le 1000000 ];
+do
+    srun Dynamic $totalNums >> Dynamic.txt
+    let totalNums=totalNums+100000
+done
