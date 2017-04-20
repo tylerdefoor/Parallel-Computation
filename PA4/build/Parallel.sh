335082956aef7598e7ba311f1f1ad3fbfdb58034
tylerdefoor <tdefoor@h1.cse.unr.edu>
2017-04-20 04:59:16 -0700

    Working-ish Parallel

--- PA4/build/Parallel.sh
+++ PA4/build/Parallel.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #SBATCH -n 9
-#SBATCH --mem=2048MB
 #SBATCH --time=00:05:00
 #SBATCH --mail-user=tylerleedefoor@gmail.com
 #SBATCH --mail-type=ALL
 
-srun Parallel 1000 >> Parallel.txt
+srun Parallel 9 >> Parallel.txt
--- PA4/src/Parallel.cpp
+++ PA4/src/Parallel.cpp
@@ -23,7 +23,7 @@ using namespace std;
 int getRow ( int taskid, int sqrWorldSize );
 int getCol ( int taskid, int sqrWorldSize );
 int getID ( int row, int col, int sqrWorldSize );
-int get2d ( int row, int col );
+int get2d ( int row, int col, int width );
 void sendToSlaves ( int worldSize, int** left, int** right, int width );
 int shift ( int destRow, int destCol, vector<int>& source, int sqrWorldSize );
 void multiply ( vector<int> left, vector<int> right, int** result, int row, int col, int width );
@@ -42,8 +42,8 @@ int main ( int argc, char** argv )
     int taskid, sqrWorldSize, worldSize, myRow, myCol, procWidth, myLeft, myUp;
 
     //Vectors to hold the left and right 
-    vector<int> left (  );
-    vector<int> right (  );    
+    vector<int> left;
+    vector<int> right;    
 
     //Everyone has their representation of the resulting vector
     int** result;
@@ -61,6 +61,8 @@ int main ( int argc, char** argv )
 
     //The start, end, and total time
     double start, end, total;
+
+    start = 0.0;
 
     /* End of Variable Declarations */
 
@@ -82,6 +84,10 @@ int main ( int argc, char** argv )
 
     //This is the width of each processor's left and right matrix
     procWidth = width / sqrWorldSize;
+
+
+    left.resize ( procWidth * procWidth );
+    right.resize ( procWidth * procWidth );
 
     //If we are the master
     if ( taskid == MASTER )
@@ -119,16 +125,19 @@ int main ( int argc, char** argv )
         {
             for ( int j = 0; j < procWidth; j++ )
             {
-                left.push_back ( templeft[i][j] );
-                right.push_back ( tempright[i][j] );
+                left[get2d(i,j,procWidth)] = templeft[i][j];
+                right[get2d(i,j,procWidth)] = tempright[i][j];
             }
         }
+
 
         //Send the slaves their submatrices
         sendToSlaves ( worldSize, templeft, tempright, procWidth );
 
+
         //Free up the space for templeft and tempright, as they are now useless
         //I will forever remember you Temple FT
+        /*
         for ( int i = 0; i < width; i++ )
         {
             delete [] templeft[i];
@@ -137,7 +146,10 @@ int main ( int argc, char** argv )
         delete [] templeft;
         delete [] tempright;
 
+        cout << "Cleared left and right" << endl;
+*/
         MPI_Barrier ( MPI_COMM_WORLD );
+
 
         //Start the timer
         start = MPI_Wtime (  );
@@ -146,8 +158,8 @@ int main ( int argc, char** argv )
     else
     {
         //Receive the left and the right from master
-        MPI_Recv ( &left[0], procWidth * procWidth, MPI_INT, MASTER, MPI_ANY_TAG, 0, MPI_STATUS_IGNORE );
-        MPI_Recv ( &right[0], procWidth * procWidth, MPI_INT, MASTER, MPI_ANY_TAG, 0, MPI_STATUS_IGNORE );
+        MPI_Recv ( &left.front (  ), width * width, MPI_INT, MASTER, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE );
+        MPI_Recv ( &right.front (  ), width * width, MPI_INT, MASTER, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE );
 
         MPI_Barrier ( MPI_COMM_WORLD );
     }    
@@ -176,6 +188,7 @@ int main ( int argc, char** argv )
         shift ( myRow, destCol, left, sqrWorldSize );
     }
 
+
     //Preprocess the right matrix
     if ( myCol != 0 )
     {
@@ -190,17 +203,23 @@ int main ( int argc, char** argv )
         shift ( destRow, myCol, right, sqrWorldSize );
     }
 
-    for ( int i = 0; i sqrWorldSize - 1; i++ )
+
+    for ( int i = 0; i <  sqrWorldSize - 1; i++ )
     {
         //Multiply the matrices and put them in result
         multiply ( left, right, result, myRow * procWidth, myCol * procWidth, procWidth );
 
+        MPI_Barrier ( MPI_COMM_WORLD );
+
         //Shift A to the left
         shift ( myRow, myLeft, left, sqrWorldSize );
 
+        
         //Shift B to the right
         shift ( myUp, myCol, right, sqrWorldSize );
     }
+
+    MPI_Barrier ( MPI_COMM_WORLD );
 
     if ( taskid == MASTER )
     {
@@ -295,26 +314,31 @@ void sendToSlaves ( int worldSize, int** left, int** right, int width )
     vector<int> templeft ( width * width );
     vector<int> tempright ( width * width );
 
+
     //Loop through all the slaves, starting at taskid 1
     for ( int currentSlave = 1; currentSlave < worldSize; currentSlave++ )
     {
-        destRow = getRow ( currentSlave );
-        destCol = getRow ( currentSlave );
+        destRow = getRow ( currentSlave, worldSize );
+        destCol = getCol ( currentSlave, worldSize );
 
         //Loop through the left and right matrices
         //All slaves get an equal portion, which starts at their column * width and row * width
-        for ( int i = destRow * width; i < i + width; i++ )
+        for ( int i = destRow * width; i < destRow * ( 2 * width ); i++ )
         {
-            for ( int j = destCol * width; j < j + width; j++ )
+            for ( int j = destCol * width; j < destCol * ( 2 * width ); j++ )
             {
                 templeft.push_back ( left[i][j] );
                 tempright.push_back ( right[i][j] );
             }
         }
 
+
         //Send the left and then the right
         MPI_Send ( &templeft.front (  ), templeft.size (  ), MPI_INT, currentSlave, 0, MPI_COMM_WORLD );
+
+
         MPI_Send ( &tempright.front (  ), tempright.size (  ), MPI_INT, currentSlave, 0, MPI_COMM_WORLD );
+
     }
 }
 
@@ -336,9 +360,10 @@ int shift ( int destRow, int destCol, vector<int>& source, int sqrWorldSize )
     //Calculate the destination
     int dest = getID ( destRow, destCol, sqrWorldSize );
 
+
     //Send and receive, using source as the only buffer
     //If you're reading this, use this method to be SUPER DUPER EFFICIENT
-    MPI_Sendrecv_replace ( &source.front (  ), source.size (  ), MPI_INT, dest, 0, MPI_ANY_SOURCE,
+    MPI_Sendrecv_replace ( &source.front (  ), source.size (  ) * 2, MPI_INT, dest, 0, MPI_ANY_SOURCE,
                             MPI_ANY_TAG, MPI_COMM_WORLD, &status );
 
     //Return the tag from the sender, just for debugging purposes
@@ -368,7 +393,9 @@ void multiply ( vector<int> left, vector<int> right, int** result, int row, int 
         {
             //Multiply it
             for ( int k = 0; k < width; k++ )
-                result[i + row][j + col] += left[get2d(i,j)] * right[get2d(i,j)];
+            {
+                result[i + row][j + col] += left[get2d ( i, k, width )] * right[get2d ( k, j, width )];
+            } 
         }
     }
 }
