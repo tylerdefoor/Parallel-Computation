5d51c6135d638ab0976b35c6d062c2441dff6db6
tylerdefoor <tdefoor@h1.cse.unr.edu>
2017-04-26 19:28:38 -0700

    Weird Preprocessing Issue

--- PA4/src/Parallel.cpp
+++ PA4/src/Parallel.cpp
@@ -1,8 +1,8 @@
-/** @file Sequential.cpp
-  * @brief A sequential implementation of Bucket Sort. The program assumes the matrices are square and there are a square number of processors
-  * @author A Mystery - But they sure are handsome and a great programmer
-  * @date 4/18/2017
-  * @version 1.0
+/** @file Parallel.cpp
+  * @brief A sparallel implementation of Matrix Multiplication. The program assumes the matrices are square and there are a square number of processors
+  * @author Tyler DeFoor
+  * @date 4/25/2017
+  * @version 1.3
   */
  
 #include <iostream>
@@ -25,7 +25,7 @@ int getCol ( int taskid, int sqrWorldSize );
 int getID ( int row, int col, int sqrWorldSize );
 int get2d ( int row, int col, int width );
 void sendToSlaves ( int worldSize, int** left, int** right, int width );
-int shift ( int destRow, int destCol, int* source, int sqrWorldSize, int width );
+int shift ( int destRow, int destCol, int* source, int sqrWorldSize, int width, int myRow, int myCol );
 void multiply ( int* left, int* right, int** result, int row, int col, int width );
 
 //The main function - Boom. There's some documentation
@@ -40,12 +40,6 @@ int main ( int argc, char** argv )
     //The taskID, square root of the world size, the world size, row and column, width of the left and right arrays for processes
     //and the left and up partners for the processes
     int taskid, sqrWorldSize, worldSize, myRow, myCol, procWidth, myLeft, myUp;
-
-    //Vectors to hold the left and right 
-    /*vector<int> left;
-    vector<int> right;*/
-
-      
 
     //Everyone has their representation of the resulting vector
     int** result;
@@ -88,7 +82,13 @@ int main ( int argc, char** argv )
     procWidth = width / sqrWorldSize;
 
     int* left = new int[procWidth*procWidth];
-    int* right = new int[procWidth*procWidth];  
+    int* right = new int[procWidth*procWidth]; 
+
+    for ( int i = 0; i < procWidth * procWidth; i++ )
+    {
+        left[i] = 0;
+        right[i] = 0;
+    }
     
     //If we are the master
     if ( taskid == MASTER )
@@ -116,8 +116,11 @@ int main ( int argc, char** argv )
         {
             for ( int col = 0; col < width; col++ )
             {
+                /*
                 templeft[row][col] = rand (  ) % MAX;
-                tempright[row][col] = rand (  ) % MAX;
+                tempright[row][col] = rand (  ) % MAX;*/
+                templeft[row][col] = row * width + col;
+                tempright[row][col] = row * width + col;
             }
         }
 
@@ -152,7 +155,6 @@ int main ( int argc, char** argv )
                 right[get2d(i,j,procWidth)] = tempright[i][j];
             }
         }
-
 
         //Send the slaves their submatrices
         sendToSlaves ( sqrWorldSize, templeft, tempright, procWidth );
@@ -205,15 +207,14 @@ int main ( int argc, char** argv )
         if ( destCol < 0 )
             destCol += sqrWorldSize;
 
-                cout << myRow << "," << myCol << " shifting left to " << myRow << "," << destCol << endl;
-
-        }
+        cout << myRow << "," << myCol << " sending left " << myRow << " times to " << myRow << "," << destCol << endl;
 
         //Shift the left matrix to its destination, which is the process' row number to the left
         //  with wraping
-        shift ( myRow, destCol, left, sqrWorldSize, procWidth );
+        shift ( myRow, destCol, left, sqrWorldSize, procWidth, myRow, myCol );
     }
 
+    cout << myRow << "," << myCol << " left after pre: " << left[0] << endl;
 
     //Preprocess the right matrix
     if ( myCol != 0 )
@@ -224,15 +225,15 @@ int main ( int argc, char** argv )
         if ( destRow < 0 )
             destRow += sqrWorldSize;
 
-                cout << myRow << "," << myCol << " shifting up to " << destRow << "," << myCol << endl;
-        }
+        cout << myRow << "," << myCol << " sending up " << myCol << " times to " << destRow << "," << myCol << endl;
         //Shift the right matrix to its destination, which is the process' column number up
         //  with wraping
-        shift ( destRow, myCol, right, sqrWorldSize, procWidth );
+        shift ( destRow, myCol, right, sqrWorldSize, procWidth, myRow, myCol );
     }
 
+    cout << myRow << "," << myCol << " right after pre: " << right[0] << endl;
 
-    for ( int i = 0; i <  sqrWorldSize - 1; i++ )
+    for ( int i = 0; i <  sqrWorldSize; i++ )
     {
         //Multiply the matrices and put them in result
         multiply ( left, right, result, myRow * procWidth, myCol * procWidth, procWidth );
@@ -240,20 +241,19 @@ int main ( int argc, char** argv )
         MPI_Barrier ( MPI_COMM_WORLD );
 
         //Shift A to the left
-        shift ( myRow, myLeft, left, sqrWorldSize, procWidth );
+        shift ( myRow, myLeft, left, sqrWorldSize, procWidth, myRow, myCol );
 
-        
         //Shift B to the right
-        shift ( myUp, myCol, right, sqrWorldSize, procWidth );
+        shift ( myUp, myCol, right, sqrWorldSize, procWidth, myRow, myCol );
     }
 
     MPI_Barrier ( MPI_COMM_WORLD );
-
+/*
     for ( int current = 0; current < worldSize; current++ )
     {
         if ( taskid == current )
         {
-            cout << "Task id result matrix" << current << endl;
+            cout << "Result matrix " << current << endl << endl;
 
             for ( int i = 0; i < width; i++ )
             {
@@ -263,11 +263,12 @@ int main ( int argc, char** argv )
                 }
                 cout << endl;
             }
+            cout << endl;
         }
 
         MPI_Barrier ( MPI_COMM_WORLD );
     }
-
+*/
     if ( taskid == MASTER )
     {
         //End the timer
@@ -277,7 +278,7 @@ int main ( int argc, char** argv )
         total = end - start;
 
         //Output the time for totalNums
-        cout << width << " " << total << endl;
+//        cout << width << " " << total << endl;
     }
     
     //Finalize MPI
@@ -368,15 +369,20 @@ void sendToSlaves ( int worldSize, int** left, int** right, int width )
         destRow = getRow ( currentSlave, worldSize );
         destCol = getCol ( currentSlave, worldSize );
 
+        int currRow = 0;
+        int currCol = 0;
         //Loop through the left and right matrices
         //All slaves get an equal portion, which starts at their column * width and row * width
         for ( int i = destRow * width; i < destRow +  width; i++ )
         {
+            currCol = 0;
             for ( int j = destCol * width; j < destCol + width; j++ )
             {
-                templeft[get2d(i,j,width)] = left[i][j];
-                tempright[get2d(i,j,width)] = right[i][j];
+                templeft[get2d(currRow,currCol,width)] = left[i][j];
+                tempright[get2d(currRow,currCol,width)] = right[i][j];
+                currCol++;
             }
+            currRow++;
         }
 
         //Send the left and then the right
@@ -396,17 +402,22 @@ void sendToSlaves ( int worldSize, int** left, int** right, int width )
  *@pre There is a process with row and column destRow and destCol that will eventually receive and something will eventually send to this process
  *@post The vector source holds the information passed from another process. No other parameters are changed
  */
-int shift ( int destRow, int destCol, int* source, int sqrWorldSize, int width )
+int shift ( int destRow, int destCol, int* source, int sqrWorldSize, int width, int myRow, int myCol )
 {
     MPI_Status status;
 
     //Calculate the destination
     int dest = getID ( destRow, destCol, sqrWorldSize );
+    int myID = getID ( myRow, myCol, sqrWorldSize );
+
+//    cout << myID << " Sent " << source[0] << " to " << dest << endl;
 
     //Send and receive, using source as the only buffer
     //If you're reading this, use this method to be SUPER DUPER EFFICIENT
     MPI_Sendrecv_replace ( source, width*width, MPI_INT, dest, 0, MPI_ANY_SOURCE,
                             MPI_ANY_TAG, MPI_COMM_WORLD, &status );
+
+//    cout << myID << " Received " << source[0] << " from " << dest << endl;
 
     //Return the tag from the sender, just for debugging purposes
     return status.MPI_TAG;
